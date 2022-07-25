dae195990f50c7571538aa46463a99268854c126
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-25 21:44:31 +0300

    Создание прилоежния users и перехват logout

---
+++ yatube/users/urls.py
@@ -0,0 +1,16 @@
+from re import template
+from django.conf import UserSettingsHolder
+from django.contrib.auth.views import LogoutView
+
+from django.urls import path
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path(
+        'logout/',
+        LogoutView.as_view(template_name = 'users/logged_out.html'),
+        name = 'logout'
+    ),
+]
