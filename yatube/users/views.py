de6b80cfa766ea4a3fd4b61624b84f921301f3e3
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-29 19:40:05 +0300

    Добавлен класс SignUp для регистрационной формы

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+#Импорт CreateView, чтобы создать ему наследника
+from django.views.generic import CreateView
 
-# Create your views here.
+#Функция reverse_lazy позволяет получить URL по параметрам ф-ции path()
+from django.urls import reverse_lazy
+
+#Импортиуем класс формы, чтобы сослаться на неё во view-классе
+from .forms import CreationForm 
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    #перенаправление на главную страницу после успешной регистрации
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
