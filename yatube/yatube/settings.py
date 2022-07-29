5fb2b5b26ca19fdb10744ad9b7d670d79b35776c
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-29 19:42:32 +0300

    Докинуто приложение core

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
