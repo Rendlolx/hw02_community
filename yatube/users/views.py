be32d78a3a7499e9b08be985b651732c5aa81d91
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-30 14:59:01 +0300

    Доавблена вьюха для смены пароля

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,15 +1,23 @@
 #Импорт CreateView, чтобы создать ему наследника
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 #Функция reverse_lazy позволяет получить URL по параметрам ф-ции path()
 from django.urls import reverse_lazy
 
 #Импортиуем класс формы, чтобы сослаться на неё во view-классе
-from .forms import CreationForm 
+from .forms import CreationForm, PasswordChangeForm, PasswordResetForm
 
 
 class SignUp(CreateView):
     form_class = CreationForm
     #перенаправление на главную страницу после успешной регистрации
-    success_url = reverse_lazy('posts:index')
+    success_url = reverse_lazy('posts:main')
     template_name = 'users/signup.html'
+
+
+class ChangePassword(UpdateView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('posts:main')
+    template_name = 'users/password_change_form.html'
+
+
