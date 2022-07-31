ea97b9edc3af421e5d1ccc9bc2ff9b63642965df
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-31 15:47:59 +0300

    Попытка переопределения смены пароля после сброса

--- yatube/users/models.py
+++ yatube/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ChangePasswordAfterReset(models.Model):
+    new_pass = models.CharField(max_length=40)
+    new_pass_confirm = models.CharField(max_length=40)
+    
