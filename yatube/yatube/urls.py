b5f3431b9fabfd165c3f28dc8e606fd45fb64628
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-08-04 01:18:26 +0300

    Добавлен url about

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
     path('auth/', include('users.urls')),
-    path('auth/', include('django.contrib.auth.urls'))
+    path('auth/', include('django.contrib.auth.urls')),
+    path('about/', include('about.urls', namespace = 'about'))
 ]
